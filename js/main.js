7a4b312013f3668b47f7b186e2cc36b59154f773
paunin <d.m.paunin@gmail.com>
2015-11-14 23:30:26 +0700

    Site

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 var initImgSize = 200;
+var curImageSize = initImgSize;
+var minOpacity = 0.2;
+
 function resize() {
     var viewportWidth = $(window).width();
-    var viewportHeight = $(window).height();
 
     var div = Math.floor(viewportWidth / initImgSize);
     var rem = viewportWidth % initImgSize;
 
-    var newSize = initImgSize + rem / div;
+    curImageSize = initImgSize + rem / div;
 
-    console.log(newSize);
-    $('.img-bgr').width(newSize).height(newSize);
+    $('.img-bgr').width(curImageSize).height(curImageSize);
 }
 $(document).ready(function () {
 
 
     for (var i = 0; i < 120; i++) {
-        $('#background').append('<div class="img-bgr" data-img-number="' + i + '"></div>')
+        $('#background').append('<div id="img_bgr_' + i + '" class="img-bgr img-bgr-' + i + '" data-img-number="' + i + '"></div>')
     }
     resize();
 
@@ -25,11 +26,39 @@ $(document).ready(function () {
 
     $(".img-inst").on("load", function () {
         $(this).parent().animate({
-            opacity: 0.9
+            opacity: minOpacity
         }, 3000);
     }).each(function () {
         if (this.complete) {
             $(this).trigger("load")
+        }
+    });
+
+
+    var currentMousePos = {x: -1, y: -1};
+    var lastSeen;
+    $(document).mousemove(function (event) {
+        var viewportWidth = $(window).width();
+        var numImages = Math.floor(viewportWidth / curImageSize);
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+
+        var column = Math.floor(currentMousePos.x / curImageSize);
+        var row = Math.floor(currentMousePos.y / curImageSize);
+
+        imgNum = row * numImages + column;
+
+        if (lastSeen != imgNum) {
+
+
+            $('#img_bgr_' + lastSeen).animate({
+                opacity: minOpacity
+            }, 500);
+
+            lastSeen = imgNum;
+            $('#img_bgr_' + lastSeen).animate({
+                opacity: 1
+            }, 1000);
         }
     });
 
